e34e30ff5e43e3d8d451e55103229cfafe22b9be
P.V.J.Raju <pvjraju26@gmail.com>
2025-12-28 18:00:45 +0530

    Committing the initial changes.

---
+++ src/main/java/com/pvj/tinderaibackend/TinderAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pvj.tinderaibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinderAiBackendApplication {
+
+    public static void main(String[] args) {
+        System.out.println("TinderAiBackendApplication this  is looking good. this is okay.");
+        SpringApplication.run(TinderAiBackendApplication.class, args);
+    }
+}
