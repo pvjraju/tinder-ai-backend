f788398fd02ef69a8db871697fbc4f2ae50b7848
P.V.J.Raju <pvjraju26@gmail.com>
2026-01-30 22:04:23 +0530

    Added a hard code profile and save it to the mongoDB to test the connection.

--- src/main/java/com/pvj/tinderaibackend/TinderAiBackendApplication.java
+++ src/main/java/com/pvj/tinderaibackend/TinderAiBackendApplication.java
@@ -1,13 +1,40 @@
 package com.pvj.tinderaibackend;
 
+import com.pvj.tinderaibackend.profiles.Gender;
+import com.pvj.tinderaibackend.profiles.Profile;
+import com.pvj.tinderaibackend.profiles.ProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+
 @SpringBootApplication
-public class TinderAiBackendApplication {
+public class TinderAiBackendApplication implements CommandLineRunner {
+
+    @Autowired
+    private ProfileRepository profileRepository;
 
     public static void main(String[] args) {
-        System.out.println("TinderAiBackendApplication this  is looking good. this is okay.");
         SpringApplication.run(TinderAiBackendApplication.class, args);
+    }
+
+    public void run(String... args) throws Exception {
+        Profile profile = new Profile(
+                "1",
+                "John",
+                "Doe",
+                28,
+                "Caucasian",
+                Gender.MALE,
+                "Love hiking and coffee",
+                "https://example.com/image.jpg",
+                "ENFP"
+        );
+
+        profileRepository.save(profile);
+        System.out.println("Profile created: " + profile);
+        profileRepository.findAll().forEach(System.out::println);
     }
 }
